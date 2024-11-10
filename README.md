5d377c59677e7e67a7d8422a95d59d1d1e5944dd
Anthony Saliba <anthony.s2501protonmail.com@proton.me>
2024-11-10 12:46:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sky Cloud
+
+SaaS website for a cloud storage service built with React & TailwindCSS.
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Home = () => {
+  return (
+    <>
+      <div className="w-full min-h-dvh m-auto flex flex-col justify-center items-center p-4">
+        <h1 className="text-4xl md:text-6xl font-bold text-blue-500 select-none">
+          Sky Cloud
+        </h1>
+      </div>
+    </>
+  );
+};
